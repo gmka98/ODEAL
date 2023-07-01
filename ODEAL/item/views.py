c8d533334083810489bd8eefb62c649034f6053a
gmk4 <gmka98@gmail.com>
2023-07-01 22:17:01 +0200

    adding items done

--- ODEAL/item/views.py
+++ ODEAL/item/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Item
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+from .models import Item
+from .forms import NewItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -10,3 +13,21 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items
     })
+
+@login_required
+def new(request):
+    if request.method == "POST":
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+           item = form.save(commit=False)
+           item.created_by = request.user
+           item.save()
+
+           return redirect('item:detail', pk=item.id)
+        else:
+           form = NewItemForm()
+
+    return render(request, 'item/form.html',{
+        'form': form
+        })
