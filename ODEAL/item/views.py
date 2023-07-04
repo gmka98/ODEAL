0e6a37dc68c0c9acfa8d06d580922d3a4fd827fe
gmk4 <gmka98@gmail.com>
2023-07-05 00:00:37 +0200

    almost done

--- ODEAL/item/views.py
+++ ODEAL/item/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 # Create your views here.
 
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if query:
+        items = items.filter(Q(name_contains=query) | Q(description__icontains=query))
+
+    if category_id:
+        items = items.filter(category_id=category_id  )
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id' :int(category_id),
+   
+         })    
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
