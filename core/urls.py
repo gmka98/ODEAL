18e6f89850f85aa71400f6fa2a981e88721376d8
gmk4 <gmka98@gmail.com>
2023-10-27 10:39:52 +0200

    odeal working

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('odeal/', views.odeal, name='odeal'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
--- core/views.py
+++ core/views.py
@@ -5,10 +5,14 @@ from item.models import Category, Item
 from .forms import SignupForm
 
 def index(request):
+    return render(request, 'core/index.html')
+   
+
+def odeal(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
-    return render(request, 'core/index.html', {
+    return render(request, 'core/odeal.html', {
         'categories': categories,
         'items': items,
     })
